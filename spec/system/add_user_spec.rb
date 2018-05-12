dd2f50b22886a79ced041824dd0f52b5ff54efa6
ToBeAmped <martinja@oregonstate.edu>
2018-05-12 12:06:25 -0700

    Add feature test for signing up as a new user

---
+++ spec/system/add_user_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "adding a user", type: :system do
+  it "allows a user to sign up" do
+    visit sign_up_user_path
+    fill_in "First name", with: "First"
+    fill_in "Last name", with: "Last"
+    fill_in "Email", with: "Fake@fake.com"
+    fill_in "Password", with: "Password"
+    click_on("Sign Up")
+    visit users_path
+    expect(page).to have_content("First")
+    expect(page).to have_content("Last")
+    expect(page).to have_content("Fake@fake.com")
+    expect(page).to have_content("Password")
+  end
+end

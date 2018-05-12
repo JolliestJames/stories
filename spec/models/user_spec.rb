01bdeb363c7c6f12641ceb4e3be2aa7cfe484a2f
ToBeAmped <martinja@oregonstate.edu>
2018-05-12 07:24:28 -0700

    Refactor initialization test into initialization describe block

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User do
   let(:user) { User.new("Fake_first", "Fake_last", "fake@fake.com", "fake_password") }
   let(:memory) { "This is a memory" }
 
-  it "knows a new user has a first and last name, an email and a password" do
-    expect(user.first_name).not_to be_nil
-    expect(user.last_name).not_to be_nil
-    expect(user.email).not_to be_nil
-    expect(user.password).not_to be_nil
+  describe "initialization" do
+    specify { expect(user.first_name).not_to be_nil }
+    specify { expect(user.last_name).not_to be_nil }
+    specify { expect(user.email).not_to be_nil }
+    specify { expect(user.password).not_to be_nil }
   end
-
+  
   it "knows a user can add a memory" do
     user.memories << memory
     expect(user).to have_memories

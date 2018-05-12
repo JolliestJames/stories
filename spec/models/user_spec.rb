aa9a42eac44d5379666398155a245ed12eaad04e
ToBeAmped <martinja@oregonstate.edu>
2018-05-12 09:04:24 -0700

    Change user spec to assert that users have stories, not memories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe User do
   let(:user) { User.new("Fake_first", "Fake_last", "fake@fake.com", "fake_password") }
-  let(:memory) { "This is a memory" }
+  let(:story) { "This is a story" }
 
   describe "initialization" do
     specify { expect(user.first_name).not_to be_nil }
@@ -10,10 +10,10 @@ RSpec.describe User do
     specify { expect(user.email).not_to be_nil }
     specify { expect(user.password).not_to be_nil }
   end
-  
-  it "knows a user can add a memory" do
-    user.memories << memory
-    expect(user).to have_memories
+
+  it "knows a user can add a story" do
+    user.stories << story
+    expect(user).to have_stories
   end
 
 end

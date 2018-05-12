100786c136600613d89c66a612b9d15d688dadb9
ToBeAmped <martinja@oregonstate.edu>
2018-05-11 22:44:30 -0700

    Add User attributes and unit test for User initialization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User
-  attr_accessor :memories
+  attr_accessor :memories, :first_name, :last_name, :email, :password
 
-  def initialize
+  def initialize(first_name, last_name, email, password)
     @memories = []
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @password = password
   end
 
   def has_memories?
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User do
-  let(:user) { User.new }
+  let(:user) { User.new("Fake_first", "Fake_last", "fake@fake.com", "fake_password") }
   let(:memory) { "This is a memory" }
+
+  it "knows a new user has a first and last name, an email and a password" do
+    expect(user.first_name).not_to be_nil
+    expect(user.last_name).not_to be_nil
+    expect(user.email).not_to be_nil
+    expect(user.password).not_to be_nil
+  end
 
   it "knows a user can add a memory" do
     user.memories << memory

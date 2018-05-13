7f53375369a9c2109043c8f25429a89bd75d79f9
ToBeAmped <martinja@oregonstate.edu>
2018-05-13 11:50:28 -0700

    Update story spec

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe Story do
     description: "My favorite story.") }
   
   let(:memory) { Memory.new(
-    "The day I met her",
-    "8/13/2017",
-    "link_to_picture",
-    "An impromptu breakfast leads to me meeting my future wife.") }
+    name: "The day I met her",
+    date: Date.new(2017, 8, 13),
+    image: "link_to_picture",
+    description: "An impromptu breakfast leads to me meeting my future wife.") }
 
   describe "initialization" do
     specify { expect(story.name).to eq("Mr. and Mrs. Martinez") }
     specify { expect(story.description).to eq("My favorite story.")}
   end
 
-  # it "allows us to add a memory" do
-  #   story.add_memory(memory)
-  #   expect(story.memories).not_to be_empty
-  # end
+  it "allows a story to add a memory" do
+    story.add_memory(memory)
+    expect(story.memories).not_to be_empty
+  end
 
 end

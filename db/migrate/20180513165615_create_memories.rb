3b355c60a782ed71a06a8161dd8d8c1044408e0a
ToBeAmped <martinja@oregonstate.edu>
2018-05-13 10:00:15 -0700

    Add migrations for user, story and memory models

---
+++ db/migrate/20180513165615_create_memories.rb
@@ -0,0 +1,14 @@
+class CreateMemories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memories do |t|
+      t.references :story, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string : name
+      t.date :date
+      t.string :image
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end

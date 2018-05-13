f51645adbeb453b131ce75d82665ff42f545d6ea
ToBeAmped <martinja@oregonstate.edu>
2018-05-13 11:19:38 -0700

    Add inheritance from ApplicationRecord to Story model, update story spec

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,14 +1,11 @@
-class Story
-  attr_accessor :memories, :name, :description
-
-  def initialize(name, description)
-    @memories = []
-    @name = name
-    @description = description
-  end
+class Story < ApplicationRecord
+  # belongs_to :user
+  has_many :memories, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
 
   def add_memory(memory)
-    @memories << memory
+    self.memories << memory
   end
 
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,5 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Story, type: :model do
+RSpec.describe Story do
+  let(:story) { Story.new(
+    name: "Mr. and Mrs. Martinez",
+    description: "My favorite story.") }
   
+  let(:memory) { Memory.new(
+    "The day I met her",
+    "8/13/2017",
+    "link_to_picture",
+    "An impromptu breakfast leads to me meeting my future wife.") }
+
+  describe "initialization" do
+    specify { expect(story.name).to eq("Mr. and Mrs. Martinez") }
+    specify { expect(story.description).to eq("My favorite story.")}
+  end
+
+  # it "allows us to add a memory" do
+  #   story.add_memory(memory)
+  #   expect(story.memories).not_to be_empty
+  # end
+
 end

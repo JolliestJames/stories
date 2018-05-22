a98ba99284e7ca32743626d4eea250bdfdc686e3
ToBeAmped <martinja@oregonstate.edu>
2018-05-22 15:44:02 -0700

    Implement create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @new_user = User.new(
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+    )
+    @new_user.save
   end
 
   def index

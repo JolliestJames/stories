9d5a730c4f94438aa98ccdbfa494810de8187a07
ToBeAmped <martinja@oregonstate.edu>
2018-05-22 16:20:01 -0700

    Add 4 feature tests to add_user_spec

--- spec/system/add_user_spec.rb
+++ spec/system/add_user_spec.rb
@@ -15,4 +15,45 @@ RSpec.describe "adding a user", type: :system do
     expect(page).to have_content("Fake@fake.com")
     expect(page).to have_content("Password")
   end
+
+  it "does not allow a user to sign up without a first name" do
+    visit new_user_path
+    fill_in "First name", with: ""
+    fill_in "Last name", with: "Last"
+    fill_in "Email", with: "Fake@fake.com"
+    fill_in "Password", with: "Password"
+    click_on("Create User")
+    expect(page).to have_selector(".new_user")
+  end
+
+  it "does not allow a user to sign up without a last name" do
+    visit new_user_path
+    fill_in "First name", with: "First"
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "Fake@fake.com"
+    fill_in "Password", with: "Password"
+    click_on("Create User")
+    expect(page).to have_selector(".new_user")
+  end
+
+  it "does not allow a user to sign up without an email address" do
+    visit new_user_path
+    fill_in "First name", with: "First"
+    fill_in "Last name", with: "Last"
+    fill_in "Email", with: ""
+    fill_in "Password", with: "Password"
+    click_on("Create User")
+    expect(page).to have_selector(".new_user")
+  end
+
+  it "does not allow a user to sign up without a password" do
+    visit new_user_path
+    fill_in "First name", with: "First"
+    fill_in "Last name", with: "Last"
+    fill_in "Email", with: "Fake@fake.com"
+    fill_in "Password", with: ""
+    click_on("Create User")
+    expect(page).to have_selector(".new_user")
+  end
+
 end

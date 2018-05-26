15dc9b3d38da73f282a8454f14032482230c1130
ToBeAmped <martinja@oregonstate.edu>
2018-05-26 15:01:44 -0700

    Refactor add user spec

--- spec/system/add_user_spec.rb
+++ spec/system/add_user_spec.rb
@@ -3,34 +3,21 @@ require "rails_helper"
 RSpec.describe "adding a user", type: :system do
   it "allows a user to sign up" do
     visit signup_path
-    fill_in "First name", with: "First"
-    fill_in "Last name", with: "Last"
+    fill_in "User name", with: "FakeUser"
     fill_in "Email", with: "Fake@fake.com"
     fill_in "Password", with: "Password"
     click_on("Create User")
     visit users_path
-    @user = User.find_by(first_name: "First")
+    @user = User.find_by(user_name: "FakeUser")
     expect(page).to have_selector(
-      "#user_#{@user.id} .first_name", text: "First")
-    expect(page).to have_content("Last")
+      "#user_#{@user.id} .user_name", text: "FakeUser")
     expect(page).to have_content("Fake@fake.com")
     expect(page).to have_content("Password")
   end
 
-  it "does not allow a user to sign up without a first name" do
+  it "does not allow a user to sign up without a user name" do
     visit signup_path
-    fill_in "First name", with: ""
-    fill_in "Last name", with: "Last"
-    fill_in "Email", with: "Fake@fake.com"
-    fill_in "Password", with: "Password"
-    click_on("Create User")
-    expect(page).to have_selector(".new_user")
-  end
-
-  it "does not allow a user to sign up without a last name" do
-    visit signup_path
-    fill_in "First name", with: "First"
-    fill_in "Last name", with: ""
+    fill_in "User name", with: ""
     fill_in "Email", with: "Fake@fake.com"
     fill_in "Password", with: "Password"
     click_on("Create User")
@@ -39,8 +26,7 @@ RSpec.describe "adding a user", type: :system do
 
   it "does not allow a user to sign up without an email address" do
     visit signup_path
-    fill_in "First name", with: "First"
-    fill_in "Last name", with: "Last"
+    fill_in "User name", with: "FakeUser"
     fill_in "Email", with: ""
     fill_in "Password", with: "Password"
     click_on("Create User")
@@ -49,8 +35,7 @@ RSpec.describe "adding a user", type: :system do
 
   it "does not allow a user to sign up without a password" do
     visit signup_path
-    fill_in "First name", with: "First"
-    fill_in "Last name", with: "Last"
+    fill_in "User name", with: "FakeUser"
     fill_in "Email", with: "Fake@fake.com"
     fill_in "Password", with: ""
     click_on("Create User")

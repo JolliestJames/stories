87c8a37a49dba5517433ef18ee439f761c931b1b
ToBeAmped <martinja@oregonstate.edu>
2018-05-29 14:57:09 -0700

    Add controller test for getting new user path

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  describe "GET signup" do
+    it "returns http success" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
 end

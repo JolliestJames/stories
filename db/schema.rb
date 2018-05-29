c4eae643277b16d04f81ad2d9aaebf1e5dcb0871
ToBeAmped <martinja@oregonstate.edu>
2018-05-29 16:08:46 -0700

    Add ChangeUsernameColumnName migration, migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_212852) do
+ActiveRecord::Schema.define(version: 2018_05_29_222110) do
 
   create_table "memories", force: :cascade do |t|
     t.integer "story_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2018_05_26_212852) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
+    t.string "username"
   end
 
 end

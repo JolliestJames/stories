34a23099c26bd4578c83493b25af7f034dbcbad7
ToBeAmped <martinja@oregonstate.edu>
2018-06-16 12:50:31 -0700

    Remove user routes/actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,3 @@
 class UsersController < ApplicationController
 
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @new_user = User.new(
-  #     username: params[:user][:username],
-  #     email: params[:user][:email],
-  #     password: params[:user][:password],
-  #   )
-  #   result = @new_user.save
-  #   unless result
-  #     @user = @new_user
-  #     render :new
-  #   end
-  # end
-
-  # def index
-  #   @users = User.all
-  # end
-
 end

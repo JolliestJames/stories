914ebe2677760ba01c32369f4f7b756c60d98eca
ToBeAmped <martinja@oregonstate.edu>
2018-06-23 12:55:30 -0700

    Recreate stories and memories tables in devise create users migration

--- db/migrate/20180612060402_devise_create_users.rb
+++ db/migrate/20180612060402_devise_create_users.rb
@@ -47,5 +47,25 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     add_index :users, :username,             unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+
+    create_table :stories do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+
+    create_table :memories do |t|
+      t.references :story, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :name
+      t.date :date
+      t.string :image
+      t.string :description
+
+      t.timestamps
+    end
+    
   end
 end

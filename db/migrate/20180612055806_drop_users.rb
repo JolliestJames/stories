e31c13e43db49d97fa21d804d48738e755fec9c8
ToBeAmped <martinja@oregonstate.edu>
2018-06-23 12:55:12 -0700

    Remove stories and memories in drop users migration

--- db/migrate/20180612055806_drop_users.rb
+++ db/migrate/20180612055806_drop_users.rb
@@ -1,5 +1,7 @@
 class DropUsers < ActiveRecord::Migration[5.2]
   def up
+    drop_table :memories
+    drop_table :stories
     drop_table :users
   end
 
@@ -12,6 +14,25 @@ class DropUsers < ActiveRecord::Migration[5.2]
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
       t.string "username"
+    end
+
+    create_table :stories do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+    
+    create_table :memories do |t|
+      t.references :story, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :name
+      t.date :date
+      t.string :image
+      t.string :description
+
+      t.timestamps
     end
   end
 end

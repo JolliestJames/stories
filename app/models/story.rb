ebd528c7375f78877c70eba547675a4290e1c3eb
ToBeAmped <martinja@oregonstate.edu>
2018-06-30 14:43:55 -0700

    Add tests to story spec, make tests pass

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,9 @@
 class Story < ApplicationRecord
   belongs_to :user
   has_many :memories, dependent: :destroy
-  validates :name, presence: true
-  validates :description, presence: true
+  validates_presence_of :name, :description
+  validates_length_of :name, minimum: 1, maximum: 50, on: :create
+  validates_length_of :description, minimum: 1, maximum: 1024, on: :create
 
   def add_memory(memory)
     self.memories << memory
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -9,6 +9,28 @@ RSpec.describe Story do
     specify { expect(story.description).to eq("fake_story_description")}
   end
 
+  it "should validate name presence" do
+    expect(story).to validate_presence_of(:name)
+  end
+
+  it "should validate description presence" do
+    expect(story).to validate_presence_of(:description)
+  end
+
+  it "should validate name length" do
+    expect(story).to validate_length_of(:name).
+      is_at_least(1).
+      is_at_most(50).
+      on(:create)
+  end
+
+  it "should validate description length" do
+    expect(story).to validate_length_of(:description).
+      is_at_least(1).
+      is_at_most(1024).
+      on(:create)
+  end
+
   it "allows a story to add a memory" do
     story.add_memory(memory)
     expect(story.memories).not_to be_empty

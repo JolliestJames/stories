7a7bb8dc5d9bb793d095c4339c6c6819a1771ba1
ToBeAmped <martinja@oregonstate.edu>
2018-06-30 14:44:20 -0700

    Add add story spec

---
+++ spec/system/add_story_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "adding a story", type: :system do
+  let(:user) { create(:user) }
+  
+  xit "allows a user to create a story" do
+    confirm_and_log_in_as(user)
+    visit new_story_path
+    fill_in "Name", with: "Fake Story"
+    fill_in "Description", with: "Fake story description"
+    click_on("Create Story")
+    visit stories_path
+    expect(page).to have_content("Fake Story")
+  end
+
+end

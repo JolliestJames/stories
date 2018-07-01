b68bb224ac953a73b34048c267b5e0d65ecfe224
ToBeAmped <martinja@oregonstate.edu>
2018-06-30 19:36:27 -0700

    Update routes to use custom paths

--- spec/support/user_helpers.rb
+++ spec/support/user_helpers.rb
@@ -1,7 +1,7 @@
 module UserHelpers
   
   def log_in_as(user)
-    visit new_user_session_path
+    visit login_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button("Log in")

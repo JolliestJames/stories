4e301bd9c6b750be452d730b94307582d1cf1f00
ToBeAmped <martinja@oregonstate.edu>
2018-07-14 07:46:19 -0700

    Add get :new test to stories controller spec

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe StoriesController, type: :controller do
 
+  describe "GET new" do
+
+    #a user can/should only get new story if they are logged in
+    it "returns http success" do
+      get :new
+      expect(response).to be_successful
+    end
+
+  end
+
 end

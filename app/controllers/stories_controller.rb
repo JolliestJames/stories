c44a92a8bc6f5a71fc242e624533d828088be046
ToBeAmped <martinja@oregonstate.edu>
2018-07-14 08:51:33 -0700

    Add create action to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,5 +3,8 @@ class StoriesController < ApplicationController
   def new
     @story = Story.new
   end
+
+  def create
+  end
   
 end
--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe StoriesController, type: :controller do
 
   describe "GET new" do
 
-    #a user can/should only get new story if they are logged in
     it "returns http success" do
       get :new
+      expect(response).to be_successful
+    end
+
+  end
+
+  describe "POST create" do
+
+    it "returns http success" do
+      post :create
       expect(response).to be_successful
     end
 

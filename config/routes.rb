08f1fd4f8078a012a93c67d64b0493445b5d2619
ToBeAmped <martinja@oregonstate.edu>
2018-07-14 07:43:59 -0700

    Add user authentication for CRUD story routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   resources :memories
-  resources :stories
   devise_scope :user do
     get "/login" => "users/sessions#new"
     get "/logout" => "users/sessions#destroy"
     get "/register" => "users/registrations#new"
     get "/edit_profile" => "users/registrations#edit"
   end
+  authenticate :user do
+    resources :stories, only: [:new, :create, :edit, :update, :destroy]
+  end
+  resources :stories, only: [:index, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

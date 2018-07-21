c8eda47324412c668f064c0f0e9037e159b18a4e
ToBeAmped <martinja@oregonstate.edu>
2018-07-20 17:51:29 -0700

    Add unhappy path tests for adding a story

--- spec/system/add_story_spec.rb
+++ spec/system/add_story_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe "adding a story", type: :system do
     expect(page).to have_content("Fake Story")
   end
 
+  it "does not allow a user to create a story without a name" do
+    log_in_as(user)
+    visit new_story_path
+    fill_in "Name", with: ""
+    fill_in "Description", with: "Fake story description"
+    click_on("Create Story")
+    expect(page).to have_selector(".new_story")  
+  end
+
+  it "does not allow a user to create a story without a description" do
+    log_in_as(user)
+    visit new_story_path
+    fill_in "Name", with: "Fake Story"
+    fill_in "Description", with: ""
+    click_on("Create Story")
+    expect(page).to have_selector(".new_story")  
+  end
+
+  it "does not allow a user to create a story without being logged in" do
+    visit new_story_path
+    expect(page).to have_content("You need to sign in or sign up before continuing")  
+  end
+
 end

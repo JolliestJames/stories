87a8047d7c72f5ce9a199672c8c9022830d401e7
ToBeAmped <martinja@oregonstate.edu>
2018-07-20 18:00:05 -0700

    Define create action in stories controller, add method to get current user id

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,10 +5,32 @@ class StoriesController < ApplicationController
   end
 
   def create
+    
+    @user_id = current_user_id
+
+    @new_story = Story.new(
+      name: params[:story][:name],
+      description: params[:story][:description],
+      user_id: @user_id
+    )
+
+    @new_story.save
+
+    if @new_story.valid?
+      redirect_to stories_path
+    else
+      @story = @new_story
+      render :new
+    end
+
   end
 
   def index
     @stories = Story.all
+  end
+
+  def current_user_id
+    current_user.id
   end
   
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  # describe "GET signup" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to be_successful
-  #   end
-  # end
-
 end

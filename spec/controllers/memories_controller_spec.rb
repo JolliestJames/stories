973c366cc2d7cdd117a60f0a847488399811e200
ToBeAmped <martinja@oregonstate.edu>
2018-07-29 15:52:32 -0700

    Add memories controller spec tests for new, create and index

--- spec/controllers/memories_controller_spec.rb
+++ spec/controllers/memories_controller_spec.rb
@@ -2,4 +2,44 @@ require 'rails_helper'
 
 RSpec.describe MemoriesController, type: :controller do
 
+  describe "GET new" do
+
+    it "returns http success" do
+      get :new
+      expect(response).to be_successful
+    end
+
+  end
+
+  describe "GET create" do
+    let(:story) { create(:story) }
+
+    it "returns http redirect to index" do
+      @user = User.find(story.user_id)
+      sign_in(@user)
+      post :create, {
+        params: {
+          memory: {
+            name: "Fake memory",
+            description: "Fake description",
+            date: Date.new,
+            image: "Fake image",
+            story_id: story.id
+          }
+        }
+      }
+      expect(response).to redirect_to :action => :index
+    end
+
+  end
+
+  describe "GET index" do
+
+    it "returns http success" do
+      get :index
+      expect(response).to be_successful
+    end
+
+  end
+
 end

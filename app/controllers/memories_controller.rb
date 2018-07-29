d77108f6687d4f589588a7b233e37dba2e90426b
ToBeAmped <martinja@oregonstate.edu>
2018-07-29 15:59:07 -0700

    Add new, create and index actions to memories controller

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,2 +1,39 @@
 class MemoriesController < ApplicationController
+
+  def new
+    @memory = Memory.new
+  end
+
+  def create
+
+    @user_id = current_user_id
+
+    @new_memory = Memory.new(
+      name: params[:memory][:name],
+      description: params[:memory][:description],
+      date: params[:memory][:date],
+      image: params[:memory][:image],
+      story_id: params[:memory][:story_id],
+      user_id: @user_id
+    )
+
+    @new_memory.save
+
+    if @new_memory.valid?
+      redirect_to memories_path
+    else
+      @memory = @new_memory
+      render :new
+    end
+
+  end
+
+  def index
+    @memories = Memory.all
+  end
+
+  def current_user_id
+    current_user.id
+  end
+
 end

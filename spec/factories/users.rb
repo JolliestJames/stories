bcc93a953df93074b54b33dc8195b1aa639201d6
ToBeAmped <martinja@oregonstate.edu>
2018-08-12 12:02:36 -0700

    Add avatar to user factory references #3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     last_name "Fake_last"
     email "fake@fake.com"
     password "fake_password"
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/files/me.jpg'), 'image/jpeg') }
 
     trait :non_unique do
       username "non_unique_username"

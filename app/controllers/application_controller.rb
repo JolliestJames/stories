6ee553f6e46cafe56eb718bf44a104cca4d1d68a
ToBeAmped <martinja@oregonstate.edu>
2018-08-12 11:00:33 -0700

    Add attributes to devise permitted keys, add removal of test files to spec helper references #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,16 @@ class ApplicationController < ActionController::Base
   protected
  
   def configure_permitted_parameters
-    attributes = [:username, :email, :password]
+    attributes = [
+      :username, 
+      :email, 
+      :password,
+      :password_confirmation,
+      :remember_me,
+      :avatar,
+      :avatar_cache,
+      :remove_avatar
+    ]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end

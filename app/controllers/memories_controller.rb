4d2970aac9ef0dcba8f54a332f1de5e116f695b8
ToBeAmped <martinja@oregonstate.edu>
2018-09-30 16:19:03 -0700

    Update memories controller Use current date if no date is provided Use date field if no name is provided Set description it nil if left blank

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,14 +1,13 @@
 class MemoriesController < ApplicationController
-
   def new
     @memory = Memory.new
   end
 
   def create
     @new_memory = Memory.new(
-      name: params[:memory][:name],
-      description: params[:memory][:description],
-      date: params[:memory][:date],
+      name: name(params),
+      description: description(params),
+      date: date(params),
       image: params[:memory][:image],
       story_id: params[:memory][:story_id],
       user_id: current_user_id
@@ -28,8 +27,21 @@ class MemoriesController < ApplicationController
     @memories = Memory.all
   end
 
+  def date(params)
+    params[:memory][:date].blank? ? Date.today : params[:memory][:date]
+  end
+
+  def name(params)
+    params[:memory][:name].blank? ? date(params) : params[:memory][:name]
+  end
+
+  def description(params)
+    params[:memory][:description].blank? ? nil : params[:memory][:description]
+  end
+
+  private
+
   def current_user_id
     current_user.id
   end
-
 end

bd0c2e85f9bce05f4729a292fb547d30feffc160
ToBeAmped <martinja@oregonstate.edu>
2018-09-30 16:11:02 -0700

    Update memories controller spec, add post#create tests

--- spec/controllers/memories_controller_spec.rb
+++ spec/controllers/memories_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe MemoriesController, type: :controller do
-
   describe "GET #new" do
     let(:user) { create(:user) }
 
@@ -16,8 +15,11 @@ RSpec.describe MemoriesController, type: :controller do
     let(:user) { create(:user) }
     let(:story) { create(:story, user: user) }
 
-    it "returns http redirect to index" do
+    before(:each) do
       sign_in(user)
+    end
+    
+    it "returns http redirect to index" do
       post :create, {
         params: {
           memory: {
@@ -30,6 +32,66 @@ RSpec.describe MemoriesController, type: :controller do
         }
       }
       expect(response).to redirect_to :action => :index
+    end
+
+    it "uses today's date if no date is provided" do
+      post :create, {
+        params: {
+          memory: {
+            name: "Fake memory",
+            description: "Fake description",
+            date: nil,
+            image: Rack::Test::UploadedFile.new(Rails.root.join('spec/support/files/me.jpg'), 'image/jpeg'),
+            story_id: story.id
+          }
+        }
+      }
+      expect(response).to redirect_to :action => :index
+    end
+
+    it "uses the date for the name if no name is provided" do
+      post :create, {
+        params: {
+          memory: {
+            name: nil,
+            description: "Fake description",
+            date: Date.today,
+            image: Rack::Test::UploadedFile.new(Rails.root.join('spec/support/files/me.jpg'), 'image/jpeg'),
+            story_id: story.id
+          }
+        }
+      }
+      expect(response).to redirect_to :action => :index
+    end
+
+    it "allows the user to provide no description" do
+      post :create, {
+        params: {
+          memory: {
+            name: "Fake memory",
+            description: nil,
+            date: Date.today,
+            image: Rack::Test::UploadedFile.new(Rails.root.join('spec/support/files/me.jpg'), 'image/jpeg'),
+            story_id: story.id
+          }
+        }
+      }
+      expect(response).to redirect_to :action => :index
+    end
+
+    it "does not allow the user to provide no image" do
+      post :create, {
+        params: {
+          memory: {
+            name: "Fake memory",
+            description: "Fake description",
+            date: Date.today,
+            image: nil,
+            story_id: story.id
+          }
+        }
+      }
+      expect(response).to be_successful
     end
   end
 
